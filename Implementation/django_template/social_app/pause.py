de5b4bf2276002f73c51a226e967c343721930c9
ge96xis <Ball-haupt-wow>
2022-06-27 22:53:34 +0200

    created pause function headers and urls

---
+++ Implementation/django_template/social_app/pause.py
@@ -0,0 +1,35 @@
+# views file of @Kerstin
+from django.http import HttpResponse
+
+from .models import Player, Match
+
+
+def pause_game(request):
+    return
+
+
+def get_paused(request):
+    return
+
+
+def resume_game(request):
+    return
+
+
+def pause_menu_show_friends(request):
+    return
+
+
+def reset_level(request):
+    return
+
+
+def clear_reset(request):
+    return
+
+
+def exit_level(request):
+    return
+
+
+# ...
