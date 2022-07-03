c107e1d87cd0907a0bfab03ad118e9206c83ac55
ge96xis <Ball-haupt-wow>
2022-07-03 13:29:46 +0200

    restructure weather dependencies, added date field to models

--- Implementation/django_template/social_app/weather.py
+++ Implementation/django_template/social_app/weather.py
@@ -1,19 +1,9 @@
 # views file for weather functionality @Kerstin
-from django.db import models
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from datetime import date
 
 from .models import WeatherTokens, Player
 from .kerstin_utils import *
-
-
-# choices is django's equivalent of enums
-class WeatherState(models.TextChoices):
-    sun = 'sun'
-    rain = 'rain'
-    wind = 'wind'
-    thunder = 'thunder'
-    snow = 'snow'
-    none = 'none'
 
 
 def get_weather_table(request):
@@ -46,15 +36,17 @@ def get_current_weather(player: Player):
 
 def get_tokens(request):
     wt: WeatherTokens = get_weather_table(request)
-    # build string; "t0,t1,...,t4#tfriend"
     t0 = wt.token0
     t1 = wt.token1
     t2 = wt.token2
     t3 = wt.token3
     t4 = wt.token4
     tfriend = wt.friend_token
-    token_string = f'{t0},{t1},{t2},{t3},{t4}#{tfriend}'
-    return HttpResponse("0: " + token_string)
+
+    data = {"t0": t0, "t1": t1, "t2": t2, "t3": t3, "t4": t4, "tf": tfriend}
+    # data = {"t": [t0, t1, t2, t3, t4], "tf": tfriend}
+    # token_string = f'{t0},{t1},{t2},{t3},{t4}#{tfriend}'
+    return JsonResponse(data)  # set safe=False to pass any data structure
 
 
 def load_friend_token(request):
@@ -65,3 +57,40 @@ def load_friend_token(request):
     # TODO: wt.friend_token = cw
     return HttpResponse(success_message)
 
+
+def get_claim_info(request):
+    wt: WeatherTokens = get_weather_table(request)
+    # info: #tokens, #friend_tokens, unlocked_friend_token, daily_claimed, current_weather, friends_current_weather
+    token_count: int = get_number_of_tokens(wt)
+    friend_token_count: int = get_number_of_friend_tokens(wt)
+    # TODO: determine if level is high enough
+    unlocked_shared_token: bool = False
+    daily_claimed: bool = has_claimed_today(wt)
+
+
+# info helpers
+def get_number_of_tokens(wt: WeatherTokens):
+    count = 0
+    # a token is counted if it has a state
+    if not wt.token0 == WeatherState.none:
+        count += 1
+    if not wt.token1 == WeatherState.none:
+        count += 1
+    if not wt.token2 == WeatherState.none:
+        count += 1
+    if not wt.token3 == WeatherState.none:
+        count += 1
+    if not wt.token4 == WeatherState.none:
+        count += 1
+    return count
+
+
+def get_number_of_friend_tokens(wt: WeatherTokens):
+    if wt.friend_token == WeatherState.none:
+        return 0
+    return 1
+
+
+def has_claimed_today(wt):
+    # last_update: date =
+    return False
