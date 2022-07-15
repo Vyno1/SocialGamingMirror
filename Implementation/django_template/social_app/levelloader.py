ca3858334ebdbc385682b425294e8b9eba832ed9
Vyno <michael.vynogradov@tum.de>
2022-07-15 13:44:02 +0200

    implemented reload for both players

--- Implementation/django_template/social_app/levelloader.py
+++ Implementation/django_template/social_app/levelloader.py
@@ -11,8 +11,14 @@ def update_level(request):
     if not hasattr(request.user, 'player'):
         return HttpResponse(f'user is not a player')
     next_level: str = request.POST['nextLevel']
+    print(request.POST['isReload'])
+    is_reload: bool = True if request.POST['isReload'] == 'True' else False
     player = request.user.player
-    if next_level == player.scene:
+    print(is_reload)
+    print(next_level)
+    print(player.scene)
+    if next_level == player.scene and not is_reload:
+        print("help")
         return HttpResponse(f'1: Scene is already the same!')
     request.user.player.scene = next_level
     match = request.user.player.host.all()[0]
@@ -62,7 +68,6 @@ def subtract_steps(request):
     if not hasattr(request.user, 'player'):
         return HttpResponse(f'user is not a player')
     spent_steps: int = int(request.POST['stepsSpent'])
-    print(spent_steps)
     player = request.user.player
     if player.steps >= spent_steps:
         player.steps -= spent_steps
