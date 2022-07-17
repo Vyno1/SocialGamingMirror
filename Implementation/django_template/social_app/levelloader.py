b7dbbcc4d21d74ad6c88ff90fdf5513baffc87ac
ge96xis <Ball-haupt-wow>
2022-07-17 21:49:48 +0200

    reset pause in levelloader.py when resetting

--- Implementation/django_template/social_app/levelloader.py
+++ Implementation/django_template/social_app/levelloader.py
@@ -19,12 +19,15 @@ def update_level(request):
     print(next_level)
     print(player.scene)
     if next_level == player.scene and not is_reload:
-        print("help")
         return HttpResponse(f'1: Scene is already the same!')
     request.user.player.scene = next_level
     match = request.user.player.host.all()[0]
     match.current_scene = next_level
     match.sceneChanges = True
+
+    if is_reload:
+        match.is_paused = False
+
     print(player.scene)
     match.save()
     player.save()
@@ -79,4 +82,3 @@ def subtract_steps(request):
         return HttpResponse(f'0: Steps have been deducted')
     else:
         return HttpResponse(f'1: Not enough steps to be deducted')
-
