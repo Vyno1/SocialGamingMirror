be17039a53412949b4e5c342e1cdedc803a8777f
ge96xis <Ball-haupt-wow>
2022-07-20 18:45:08 +0200

    extracted table getting methods to kerstin_utils.py

--- Implementation/django_template/social_app/kerstin_utils.py
+++ Implementation/django_template/social_app/kerstin_utils.py
@@ -1,3 +1,6 @@
+from social_app.models import Player, Match
+from social_app.weatherstate import WeatherState
+
 # ----------------------------------------------------{ query errors }--------------------------------------------------
 
 not_signed_in_message = '1: user not signed in'
@@ -14,5 +17,39 @@ failed_message = '1: error'
 
 bool_true = 'true'
 bool_false = 'false'
+
+
+# -------------------------------------------------{ get match helper }-------------------------------------------------
+
+def get_match(request):
+    if not hasattr(request.user, 'player'):
+        return None
+
+    player: Player = request.user.player
+
+    player_is_host: bool = player.host.all().count() == 1
+    match: Match = Match.objects.get(host=player) if player_is_host else Match.objects.get(joined_player=player)
+
+    if not match:
+        return None
+
+    return match
+
+
+# ----------------------------------------------{ string to weather state }---------------------------------------------
+
+def string_2_weatherstate(string: str) -> WeatherState:
+    if string == "sun":
+        return WeatherState.sun
+    if string == "rain":
+        return WeatherState.sun
+    if string == "snow":
+        return WeatherState.snow
+    if string == "wind":
+        return WeatherState.wind
+    if string == "thunder":
+        return WeatherState.thunder
+
+    return WeatherState.none
 
 # --------------------------------------------------------{ END }-------------------------------------------------------
